64a99b90295a788c263e53f4804f0c5b43332502
mvl22 <martin@lucas-smith.co.uk>
2026-09-02 14:08:20 +0100

    In timestamp allocation, default empty month/day to 1, fixing bug in 9adcea14acc87e85720a406e86d28005ab95ab6f

--- symplecticPublicationsClient.php
+++ symplecticPublicationsClient.php
@@ -991,7 +991,7 @@ class symplecticPublicationsClient extends frontControllerApplication
 		
 		# Add publication timestamp, to enable easy most-recent-first sorting
 		foreach ($data as $id => $publication) {
-			$data[$id]['time'] = strtotime ($publication['publicationYear'] . '-' . str_pad ($publication['publicationMonth'], 2, '0', STR_PAD_LEFT) . '-' . str_pad ($publication['publicationDay'], 2, '0', STR_PAD_LEFT));
+			$data[$id]['time'] = strtotime ($publication['publicationYear'] . '-' . str_pad ($publication['publicationMonth'] ?: 1, 2, '0', STR_PAD_LEFT) . '-' . str_pad ($publication['publicationDay'] ?: 1, 2, '0', STR_PAD_LEFT));
 		}
 		
 		# Remove internal timestamp
